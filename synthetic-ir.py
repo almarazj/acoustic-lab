9655b6f12413842654fec807fdf13911aaa57f5d
juan <juan.almaraz097@gmail.com>
2024-03-24 03:29:17 -0300

    temporary changes

--- synthetic-ir.py
+++ synthetic-ir.py
@@ -1,98 +1,66 @@
-import os
-import librosa
 import numpy as np
 import soundfile as sf
 import scipy.signal as sig
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from scipy.ndimage import interpolation
-from scipy.signal import hilbert, find_peaks, istft, medfilt
+from scipy.signal import istft, medfilt
 from utils import *
 
-# load rirs path
-path = os.path.abspath(os.getcwd())
-irs_path= os.path.join(path,'audio-files')
-filenames = [f for f in os.listdir(irs_path) if f.endswith('.wav')]
-filenames.sort()
 
 # Load first RIR
+irs_path, filenames = load_rirs_path()
 t, rir, fs, duration = load_ir(irs_path, filenames[0])
 
-# measured IR envelope
-
-peaks, _ = find_peaks(np.abs(rir))
-
-env_peaks = np.zeros(len(rir))
-env_peaks[peaks] = np.abs(rir)[peaks]
-
-z = len(rir) / len(rir[peaks])
-
-ir_env = interpolation.zoom(np.abs(rir)[peaks], z)
-ir_env[0] = rir[0]
-
-#------------- Synthetic IR generator -------------#
-
-# M = 0.0005         # background noise amplitude
-# A = 0.055           # impulse response amplitude
-# rt = 0.5           # reverberation time
-
-# # decay rate
-# b = 3 / (rt * np.log(np.e))    
-
-# gaussian noise
-noise = np.random.normal(0, 1, int(fs*duration))
-
-
-# time vector
-t = np.linspace(0, int(duration), int(fs*duration))
-
-# synthetic IR
-syntetic_ir = (noise * ir_env) 
-n = np.max(syntetic_ir) / np.max(rir)
-normalized_synthetic_ir = syntetic_ir /n
+# Generate synthetic RIR
+norm_synt_ir = generate_synthetic_rir(rir)
 
 # Ploteos
-plt.plot(t, 0.5+rir, label='rir real')
-#plt.plot(t, 1+ir_env, label='envolvente real')
-plt.plot(t, -0.5+normalized_synthetic_ir, label='rir sintetica')
-plt.legend()
-plt.show()
+# plt.plot(t, 0.5+rir, label='rir real')
+# plt.plot(t, -0.5+norm_synt_ir, label='rir sintetica')
+# plt.legend()
+# plt.show()
 
 # espectrograma
+win_size = 256
+f, t, rir_spec = sig.stft(rir,
+                            fs=fs, 
+                            nperseg=win_size, 
+                            padded=True)
 
-f, t, Sxx = sig.spectrogram(rir, fs)
-Sxx = 20*np.log10(Sxx/2e-5)
+rir_spec_dB = np.abs(rir_spec)
 
-f, t, synt_Sxx = sig.spectrogram(normalized_synthetic_ir, fs)
-synt_Sxx = 20*np.log10(synt_Sxx/2e-5)
+_, _, synt_rir_spec = sig.stft(norm_synt_ir,
+                                fs=fs,
+                                nperseg=win_size,
+                                padded=True)
 
-frequency_response = medfilt(Sxx[:,0], 25)
+synt_rir_spec_dB = np.abs(synt_rir_spec)
 
-spec_diff = medfilt(synt_Sxx[:,0], 25) - frequency_response
+frequency_response = medfilt(rir_spec_dB[:,0], 25)
 
-synt_Sxx_new = synt_Sxx
-for i in range(np.shape(synt_Sxx)[1]):
-    synt_Sxx_new[:, i] = synt_Sxx[:, i] - spec_diff
+spec_diff = medfilt(synt_rir_spec_dB[:,0], 25) - frequency_response
 
-fig, (ax1, ax2) = plt.subplots(1, 2)
-norm = mpl.colors.Normalize(vmin=-100, vmax=0)
-mesh1 = ax1.pcolormesh(t, f, Sxx, shading='gouraud', norm=norm)
-#ax1.set_yscale('symlog')
+filt_synt_rir_spec_dB = synt_rir_spec_dB
+for i in range(np.shape(synt_rir_spec_dB)[1]):
+    filt_synt_rir_spec_dB[:, i] = synt_rir_spec_dB[:, i] - spec_diff
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+#norm = mpl.colors.Normalize(vmin=-1, vmax=1)
+mesh1 = ax1.pcolormesh(t, f, rir_spec_dB, shading='gouraud')
 ax1.set_ylim(20,20000)
-mesh2 = ax2.pcolormesh(t, f, synt_Sxx, shading='gouraud', norm=norm)
-#ax2.set_yscale('symlog')
+mesh2 = ax2.pcolormesh(t, f, synt_rir_spec_dB, shading='gouraud')
 ax2.set_ylim(20,20000)
-#ax3.pcolormesh(t, f, synt_Sxx_new, shading='gouraud')
-#ax2.set_yscale('symlog')
-#ax3.set_ylim(20,20000)
-fig.colorbar(mesh1, norm=norm)
-fig.colorbar(mesh2, norm=norm)
-#fig.colorbar(ax3.pcolormesh(t, f, synt_Sxx_new, shading='gouraud'))
+mesh3 = ax3.pcolormesh(t, f, filt_synt_rir_spec_dB, shading='gouraud')
+ax3.set_ylim(20,20000)
+fig.colorbar(mesh1)
+fig.colorbar(mesh2)
+fig.colorbar(mesh3)
+
 
 plt.show()
 
 
 # exportar archivos
-_, audio_signal = istft(synt_Sxx_new)
+_, audio_signal = istft(filt_synt_rir_spec_dB, nperseg=win_size)
 
-sf.write('RIRs/synthetic_ir.wav', audio_signal, fs)
+sf.write('audio-files/synthetic_ir.wav', audio_signal, fs)
--- utils.py
+++ utils.py
@@ -1,21 +1,52 @@
 import soundfile as sf
 import os
 import numpy as np
+from scipy import signal
+from scipy import ndimage
+
+def load_rirs_path():
+    path = os.path.abspath(os.getcwd())
+    irs_path= os.path.join(path,'audio-files')
+    filenames = [f for f in os.listdir(irs_path) if f.endswith('.wav')]
+    filenames.sort()
+    return irs_path, filenames
 
 def load_ir(path, filename):
     raw_rir, fs = sf.read(os.path.join(path, filename))
 
-    raw_duration = int(len(raw_rir) / fs)
+    raw_t = np.linspace(0, len(raw_rir), len(raw_rir))
 
-    raw_t = np.linspace(0, raw_duration, int(fs*raw_duration))
-
-    t = raw_t[0:int(fs*raw_duration)-np.argmax(np.abs(raw_rir))-1]
+    t = raw_t[0:len(raw_rir)-np.argmax(np.abs(raw_rir))-1] / fs
     rir = raw_rir[np.argmax(np.abs(raw_rir)):-1]
 
-    duration = int(len(rir) / fs)
+    duration = len(rir) / fs
     return t, rir, fs, duration
 
-def generate_synthetic_rir(t, rir, fs, duration):
+def generate_synthetic_rir(rir):
 
+    peaks, _ = signal.find_peaks(np.abs(rir))
+
+    env_peaks = np.zeros(len(rir))
+    env_peaks[peaks] = np.abs(rir)[peaks]
+
+    z = len(rir) / len(rir[peaks])
+
+    ir_env = ndimage.interpolation.zoom(np.abs(rir)[peaks], z)
+
+    # M = 0.0005         # background noise amplitude
+    # A = 0.055           # impulse response amplitude
+    # rt = 0.5           # reverberation time
+
+    # # decay rate
+    # b = 3 / (rt * np.log(np.e))    
+
+    # gaussian noise
+    noise = np.random.normal(0, 1, len(rir))
+
+    # synthetic IR
+    syntetic_ir = (noise * ir_env) 
+    n = np.max(syntetic_ir) / np.max(rir)
+    normalized_synthetic_ir = syntetic_ir / n
     
-    noise = np.random.normal(0, 1, int(fs*duration))
+    return normalized_synthetic_ir
+
