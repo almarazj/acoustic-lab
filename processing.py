1cc834db1252b72a2542d5af340a81fd017eb552
juan <juan.almaraz097@gmail.com>
2024-05-26 04:44:40 -0300

    direct + delay gap

--- processing.py
+++ processing.py
@@ -4,6 +4,10 @@ import soundfile as sf
 import os
 import scipy.signal as sig
 import pyloudnorm as pyln
+import librosa
+from dtw import dtw
+from numpy.linalg import norm
+
 
 # Functions
 
@@ -48,7 +52,6 @@ def velvetNoise(duration, fs, f, ensureLast):
             Y[p_idx+1] = round(np.random.rand()) - 1 
             print('forcing last pulse within bounds')
     Y = normalize(Y, fs, norm_level)   
-    Y[0] = 50 
     return Y
 
 def expVelvetNoise(duration, fs, initial_interval, decay_rate):
@@ -67,7 +70,6 @@ def expVelvetNoise(duration, fs, initial_interval, decay_rate):
 def gaussianNoise(duration, fs):
     Y = np.random.normal(0, 0.3, int(duration * fs))
     Y = normalize(Y, fs, norm_level)
-    Y[0] = 50
     return Y
 
 def envelope(t, rt, A, M):
@@ -137,11 +139,43 @@ def plotSpectrum(data1, data2):
     # plt.legend()
     # plt.show()
     
+def plotDtw():
+    plt.subplot(1,2,1)
+    mel1 = librosa.feature.melspectrogram(y=norm_audio_gn, sr=fs, n_mels=128, fmax=16000)
+    S1_dB = librosa.power_to_db(mel1, ref=np.max)
+    librosa.display.specshow(S1_dB)
+
+    plt.subplot(1,2,2)
+    mel2 = librosa.feature.melspectrogram(y=norm_audio_vn, sr=fs, n_mels=128, fmax=16000)
+    S2_dB = librosa.power_to_db(mel2, ref=np.max)
+    librosa.display.specshow(S2_dB)
+
+    plt.show()
+
+    dist, cost, acc_cost, path = dtw(S1_dB.T, S2_dB.T, dist=lambda x, y: norm(x - y, ord=1))
+    print('Normalized distance between the two sounds:', dist)
+
+    plt.imshow(cost.T, origin='lower', interpolation='nearest')
+    plt.plot(path[0], path[1], 'w')
+    plt.xlim((-0.5, cost.shape[0]-0.5))
+    plt.ylim((-0.5, cost.shape[1]-0.5))
+    plt.show()
+
+def DRR(rir, fs, direct_length):
+    M = len(rir)
+    N = int((direct_length/1000) * fs)
+    h2 = rir ** 2
+    direct = np.sum(h2[0:N-1])
+    reverberated = np.sum(h2[N:M])
+    DRR = 10 * np.log10(direct/reverberated)
+    return DRR
+
 duration = 2                    # seconds
 rt = 1                          # reverberation time in seconds
-f = np.arange(1,20+1,1)         # pulse density array
+f = np.arange(50, 350+1, 50)         # pulse density array
+#pulse_density = 20
 norm_level = -18                # Loudness Units relative to Full Scale
-
+direct_length = 9              # ms
 # Read anechoic audio file
 audio_data = read_audio_files()
 (data, fs) = audio_data[0]
@@ -152,22 +186,33 @@ env = envelope(t, rt, 1, 0.00001)
 # Gaussian noise generation and convolution with anechoic audio file
 gnoise = gaussianNoise(duration, fs)
 g_rir = gnoise * env
+g_rir[1:int(fs*direct_length/1000)] = 0
+g_rir[0] = 24
+direct_to_reverb = DRR(g_rir, fs, direct_length)
+print('The direct to reverberant ratio is :', direct_to_reverb, 'dB (GWN)')
 audio_gn = sig.convolve(data, g_rir)
 norm_audio_gn = normalize(audio_gn, fs, norm_level)
-sf.write('audio-files/flute/new_g_flute.wav', norm_audio_gn, fs)
+sf.write('audio-files/drums/new_g_drums.wav', norm_audio_gn, fs)
 
 # Velvet noise generation and convolution with anechoic audio file
 for pulse_density in f: 
     vnoise = velvetNoise(duration, fs, pulse_density, 1)
     v_rir = vnoise * env
+    v_rir[1:int(fs*direct_length/1000)] = 0
+    v_rir[0] = 24
+    direct_to_reverb = DRR(v_rir, fs, direct_length)
+    print('The direct to reverberant ratio is :', direct_to_reverb, 'dB (OVN)')
     audio_vn = sig.convolve(data, v_rir)
     norm_audio_vn = normalize(audio_vn, fs, norm_level)
-    sf.write('audio-files/flute/new_v%s_flute.wav' % (pulse_density), norm_audio_vn, fs)
+    sf.write('audio-files/drums/new_v%s_drums.wav' % (pulse_density), norm_audio_vn, fs)
 
 fig, axs = plt.subplots(3, 2)
 axs[0, 0].plot(t, env, 'k')
-axs[0, 1].plot(t[1:], vnoise[1:], 'k')
-axs[1, 0].plot(v_rir[1:], 'k')
+axs[0, 1].plot(t, gnoise, 'k')
+axs[1, 0].plot(g_rir, 'k')
 axs[1, 1].plot(data[0:100000], 'k')
 axs[2, 0].plot(norm_audio_gn[0:100000], 'k')
 plt.show()
+
+
+
